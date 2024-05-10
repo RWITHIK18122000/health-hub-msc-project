d8caf840a33a8145e9e077996c5964bad0756014
Rwithik Nambiar Kodakkatteri Puthiyaveedu <93855137+RWITHIK18122000@users.noreply.github.com>
2024-05-10 11:33:43 +0100

    Delete CODE directory

--- CODE/backend/app/configs/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	PORT: 5000,
-	DB_HOST: "localhost",
-	DB_USER: "root",
-	DB_PASSWORD: "",
-	DB_DATABASE: "health_hub",
-	JWT_SECRET: "67JHKNJfjfY787@#$",
-};
--- CODE/backend/app/controllers/test.controller.js
+++
@@ -1,7 +0,0 @@
-const pool = require("../libs/mysql2");
-
-module.exports.test = async (req, res, next) => {
-	const [rows] = await pool.query("SELECT * FROM categories");
-
-	res.send({ message: "Its working!!", data: rows });
-};
